7c5d348cfe6583dd192bb2f870c5e2696a6208f7
Grimcall <mccoycross@gmail.com>
2022-08-29 21:24:18 -0400

    Lots of learning. Did some UX stuff, need to work on processing csv now.

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request, send_file
+from cgitb import text
+from flask import Flask, flash, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import pandas
 
 app = Flask(__name__)
+app.secret_key = "yuhyeet"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/ready", methods = ['POST'])
+@app.route("/", methods = ['POST'])
 def ready(): 
     if request.method == 'POST': #If the form has been submitted...
-        f = request.files['file']
+        f = request.files['f_address']
+        
+        if f.filename.endswith(".csv") is False: #If file submitted is not csv...
+            error = "The file submitted is not in a .csv format. Please try again."
+            
+            return render_template("index.html", error = error)
+        
+        flash('File submitted! Processing...')
         f.save(secure_filename("uploaded" + f.filename))
-
-
-
+        
+        print(f.filename)
+        print(f)
+        print(type(f))
+    return redirect(url_for("index"))
 
 if __name__ == '__main__':
     app.debug = True
